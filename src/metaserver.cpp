4f00dd29990275232be02618cce9159709994615
Jacob Borden <amenra.beats@gmail.com>
2024-10-13 07:03:26 -0300

    Created networking framework for metadata server and cleaned up project folder

--- src/metaserver.cpp
+++ src/metaserver.cpp
@@ -6,55 +6,38 @@
 #include <vector>
 #include <string>
 #include <mutex>
-#include "filesystem.h" 
+#include "filesystem.h"
 #include "message.h" // Including message for node communication
 #include <sstream>
 #include "metaserver.h"
+#include "server.h"
+#include <thread>
 
-
-int server() {
-    MetadataManager metadataManager;
-    FileSystem fileSystem; // Using FileSystem from filesystem.h to manage file operations
-
-    // Example of adding a file with its nodes
-    std::string filename = "example.txt";
-    std::vector<std::string> nodes = {"Node1", "Node3", "Node5"};
-    if (fileSystem.createFile(filename)) {
-        fileSystem.writeFile(filename, "This is an example file content"); // Write file content
-        metadataManager.addFile(filename, nodes); // Add metadata for the file
-    } else {
-        std::cerr << "Failed to create file: " << filename << std::endl;
+void HandleClientConnection(Networking::Server _pServer, Networking::ClientConnection _pClient)
+{
+    Message request = DeserializeMessage(&_pServer.Receive(_pClient)[0]);
+    switch (request._Type)
+    {
     }
+}
 
-    // Retrieve nodes storing the file
-    try {
-        std::vector<std::string> retrievedNodes = metadataManager.getFileNodes(filename);
-        std::cout << "Nodes storing '" << filename << "': ";
-        for (const auto &node : retrievedNodes) {
-            std::cout << node << " ";
+int main()
+{
+    Networking::Server server(50505);
+
+    if (server.ServerIsRunning())
+    {
+        MetadataManager metadataManager;
+        FileSystem fileSystem; // Using FileSystem from filesystem.h to manage file operations
+        while (true)
+        {
+            // Accept a client connection
+
+            Networking::ClientConnection client = server.Accept();
+            std::thread clientThread(HandleClientConnection, server, client);
+            clientThread.detach();
         }
-        std::cout << std::endl;
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
     }
-
-    // Read the file content
-    try {
-        std::string fileContent = fileSystem.readFile(filename);
-        if (fileContent.empty()) {
-            std::cerr << "Failed to read file or file is empty: " << filename << std::endl;
-        } else {
-            std::cout << "Content of '" << filename << "': " << fileContent << std::endl;
-        }
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-
-    // Remove the file from metadata
-    metadataManager.removeFile(filename);
-
-    // Print current metadata
-    metadataManager.printMetadata();
 
     return 0;
 }
