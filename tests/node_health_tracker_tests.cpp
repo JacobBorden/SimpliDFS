8d468c61b614e4438baeaf518f6504993c71332e
Jacob N Borden <amenra.beats@gmail.com>
2025-06-07 18:49:41 -0300

    Add comments and tests for NodeHealthTracker

---
+++ tests/node_health_tracker_tests.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "metaserver/node_health_tracker.h"
+#include <thread>
+#include <chrono>
+
+TEST(NodeHealthTracker, AliveAfterSuccess) {
+    NodeHealthTracker tracker(std::chrono::seconds(1));
+    tracker.recordSuccess("node1");
+    EXPECT_FALSE(tracker.isNodeDead("node1"));
+}
+
+TEST(NodeHealthTracker, DeadAfterThreshold) {
+    NodeHealthTracker tracker(std::chrono::milliseconds(500));
+    tracker.recordSuccess("node1");
+    std::this_thread::sleep_for(std::chrono::milliseconds(600));
+    EXPECT_TRUE(tracker.isNodeDead("node1"));
+}
+
+TEST(NodeHealthTracker, ThresholdUpdate) {
+    NodeHealthTracker tracker(std::chrono::seconds(2));
+    tracker.recordSuccess("node1");
+    std::this_thread::sleep_for(std::chrono::milliseconds(800));
+    tracker.setThreshold(std::chrono::milliseconds(500));
+    EXPECT_TRUE(tracker.isNodeDead("node1"));
+}
