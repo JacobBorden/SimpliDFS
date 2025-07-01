b1501114f4e5f63d4775cd07f5f845bfc28fc0a5
Jacob Borden <amenra.beats@gmail.com>
2025-07-01 17:57:32 -0300

    fix: update log directory path to /var/simplidfs/logs

--- src/main_node.cpp
+++ src/main_node.cpp
@@ -59,7 +59,7 @@ int main(int argc, char *argv[]) {
   }
 
   try {
-    std::string logDir = "/var/logs/simplidfs";
+    std::string logDir = "/var/simplidfs/logs";
     try {
       std::filesystem::create_directories(logDir);
     } catch (...) {
